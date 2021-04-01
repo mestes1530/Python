c57a21b873ba32472bc766d894bf8cd463dc6156
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-03-31 20:21:58 -0700

    Update Inheritance_Classes.py

--- The Tech Academy/Inheritance_Classes.py
+++ The Tech Academy/Inheritance_Classes.py
@@ -5,13 +5,19 @@ class Vehicle:
 		self.electricFuel = electricFuel
 		self.numWheels = numWheels
 
-# Creates a child class called Car from Vehicle class
+# Creates a child class called Car from parent Vehicle class
 class Car(Vehicle):
 	def __init__(self, vehicleType, electricFuel, numWheels, carMaker, carModel):
 		super().__init__(self, vehicleType, electricFuel, numWheels)
 		self.carMaker = carMaker
 		self.carModel = carModel
 
+# Creates a child class called Bike from parent Vehicle class
+class Bike(Vehicle):
+	def __init__(self, vehicleType, electricFuel, numWheels, fixedGear, mountainBike):
+		super().__init__(self, vehicleType, electricFuel, numWheels)
+		self.fixedGear = fixedGear
+		self.mountainBike = mountainBike
 
 
 	
