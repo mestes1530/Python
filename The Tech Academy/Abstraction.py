92b836a84753bff56a330dc9d50f2168c3a8505d
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-04-01 21:02:47 -0700

    Add files via upload

---
+++ The Tech Academy/Abstraction.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class car(ABC):
+	def paySlip(self, amount):
+		print("Your purchase amount: ", amount)
+
+	@abstractmethod
+	def payment(self, amount):
+		pass
+
+class DebitCardPayment(car):
+	def payment(self, amount):
+		print("Your purchase amount of {} exceeded your $100 limit ".format(amount))
