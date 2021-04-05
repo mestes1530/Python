68e3f1b12d89f145a62cb959a360dca983163422
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-04-04 19:21:03 -0700

    Update File_Transfer.py

--- The Tech Academy/File_Transfer.py
+++ The Tech Academy/File_Transfer.py
@@ -2,13 +2,12 @@ import shutil
 import os
 
 # Set where the source of the files are
-source = 'https://github.com/mestes1530/Python/tree/main/The%20Tech%20Academy/sourceFolder'
+source = '/Users/mitchell_estes/Desktop/The Tech Academy/sourceFolder'
 
 # Set the destination path to destinationFolder
-destination = 'https://github.com/mestes1530/Python/tree/main/The%20Tech%20Academy/destinationFolder'
+destination = '/Users/mitchell_estes/Desktop/The Tech Academy/destinationFolder'
 files = os.listdir(source)
 
 for i in files:
 	# Move the files represented by i to the destination file
 	shutil.move(source+i, destination)
-
