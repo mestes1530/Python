f9f3f78160f05647ff95157c4b701f840fdb9f50
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-04-05 11:49:21 -0700

    Update File_Transfer.py

--- The Tech Academy/File_Transfer.py
+++ The Tech Academy/File_Transfer.py
@@ -1,13 +1,20 @@
 import shutil
 import os
 
-# Set where the source of the files are
-source = '/Users/mitchell_estes/Desktop/The Tech Academy/sourceFolder'
+# Set where the source of the files that the user should check daily for new content
+source = '/Users/mitchell_estes/Desktop/The Tech Academy/sourceFolder/'
 
-# Set the destination path to destinationFolder
-destination = '/Users/mitchell_estes/Desktop/The Tech Academy/destinationFolder'
+# Set the destination path to destinationFolder where copied files are stored
+destination = '/Users/mitchell_estes/Desktop/The Tech Academy/destinationFolder/'
+
+# Defines files that the user will have to check on a daily occurance
 files = os.listdir(source)
 
-for i in files:
-	# Move the files represented by i to the destination file
-	shutil.move(source+i, destination)
+# Function called by the user that moves files in the source folder to destination
+def File_Check():
+	# Loops through all the daily files that need to be moved to destination
+	for i in files:
+		shutil.move(source+i, destination)
+
+# Calling the class
+File_Check()
