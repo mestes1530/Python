b99c5c692c0316f7764faeb9a46e8ac69b7b7fa0
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-04-05 18:57:12 -0700

    Update File_Transfer.py

--- The Tech Academy/File_Transfer.py
+++ The Tech Academy/File_Transfer.py
@@ -10,11 +10,21 @@ destination = '/Users/mitchell_estes/Desktop/The Tech Academy/destinationFolder/
 # Defines files that the user will have to check on a daily occurance
 files = os.listdir(source)
 
+archives = os.listdir(destination)
+
 # Function called by the user that moves files in the source folder to destination
 def File_Check():
 	# Loops through all the daily files that need to be moved to destination
 	for i in files:
 		shutil.move(source+i, destination)
 
-# Calling the class
+# User prompts detailing the file transfer in text
+print("Files in sourceFolder: { " + files[0] + ", " + files[1] + ", " + files[2] + " }\n")
+print("Files in destinationFolder: { }\n")
+print("Press 'Enter' to run the daily file transfer:\n")
+x = input() # Breaks here, tried a variation of input statements
+print("Input was successfully entered")
+
+
+# Calling the function
 File_Check()
